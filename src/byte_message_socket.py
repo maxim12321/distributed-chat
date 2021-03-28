9490cf6c5d22d2078bdabfeec90153f739e5480b
Takumi <subarufallout@gmail.com>
2021-03-28 23:52:04 +0300

    Add listen function to ByteMessageSocket

--- src/byte_message_socket.py
+++ src/byte_message_socket.py
@@ -2,16 +2,29 @@ from src.byte_message_type import ByteMessageType
 from cryptography.hazmat.primitives import hashes, hmac
 import socket
 import sys
+import threading
 
 
 class ByteMessageSocket:
-    def __init__(self, on_message_received):
+    def __init__(self, on_message_received: callable):
         self.BYTE_ORDER = "big"
         self.BYTE_MESSAGE_TYPE_LENGTH = 1
         self.MESSAGE_LENGTH_LENGTH = 2
         self.PORT_ID = 8080
         self.on_message_received = on_message_received
-        # To do work with on_message_received
+        self.my_thread = threading.Thread(self.listen())
+        self.my_thread.start()
+
+    def listen(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind(('', int(self.PORT_ID)))
+        self.sock.listen(1)
+        while True:
+            listen_socket, address = self.sock.accept()
+            len = int.from_bytes(listen_socket.recv(2), self.BYTE_ORDER)
+            message = listen_socket.recv(len)
+            self.on_message_received(address, message)
+            listen_socket.close()
 
     def send(self, ip: str, message_type: ByteMessageType, message: bytes):
         message = int(message_type).to_bytes(self.BYTE_MESSAGE_TYPE_LENGTH, self.BYTE_ORDER) + message
