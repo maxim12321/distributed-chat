835892b73f8f99f7f6aa11dbdd10621030f13b36
Takumi <subarufallout@gmail.com>
2021-03-29 11:26:06 +0300

    Function send now has return type: bool

--- src/byte_message_socket.py
+++ src/byte_message_socket.py
@@ -6,17 +6,17 @@ import threading
 
 
 class ByteMessageSocket:
-    def __init__(self, on_message_received: callable):
+    def __init__(self, ip: str, on_message_received: callable):
         self.BYTE_MESSAGE_TYPE_LENGTH = 1
         self.MESSAGE_LENGTH_LENGTH = 2
         self.BYTE_ORDER = "big"
         self.PORT_ID = 8080
-        my_thread = threading.Thread(target=ByteMessageSocket.listen, args=(self, on_message_received))
+        my_thread = threading.Thread(target=ByteMessageSocket.listen, args=(self, ip, on_message_received))
         my_thread.start()
 
-    def listen(self, on_message_received: callable) -> None:
+    def listen(self, ip: str, on_message_received: callable) -> None:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.bind(('', int(self.PORT_ID)))
+        sock.bind((ip, int(self.PORT_ID)))
         sock.listen(1)
         while True:
             listen_socket, address = sock.accept()
@@ -25,12 +25,16 @@ class ByteMessageSocket:
             on_message_received(address, message)
             listen_socket.close()
 
-    def send(self, ip: str, message_type: ByteMessageType, message: bytes) -> None:
+    def send(self, ip: str, message_type: ByteMessageType, message: bytes) -> bool:
         message = int(message_type).to_bytes(self.BYTE_MESSAGE_TYPE_LENGTH, self.BYTE_ORDER) + message
         message = sys.getsizeof(message).to_bytes(self.MESSAGE_LENGTH_LENGTH, self.BYTE_ORDER) + message
         send_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        send_sock.connect((ip, self.PORT_ID))
-        send_sock.sendall(message)
+        try:
+            send_sock.connect((ip, 8080))
+            send_sock.sendall(message)
+        except:
+            return False
+        return True
 
     def add_authentivation_code(self, message: bytes, key: bytes) -> bytes:
         h = hmac.HMAC(key, hashes.SHA1())
