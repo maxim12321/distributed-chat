c1545446620579044f99d66591f47ac29ecefced
Katerina <katealexposp@gmail.com>
2021-04-04 14:30:23 +0300

    Add some changes
    Добавлен метод создания чата в ChatManager, изменено название переменной, явно указаны типы возвращаемых значений

--- src/chat_manager.py
+++ src/chat_manager.py
@@ -4,12 +4,16 @@ import constants
 
 class ChatManager:
     def __init__(self):
-        self.chat_id_to_chat = {}
+        self.chat_list = dict()
 
-    def add_chat(self, chat: Chat):
-        self.chat_id_to_chat.update({chat.get_chat_id(): chat})
+    def add_chat(self, chat: Chat) -> None:
+        self.chat_list[chat.get_chat_id()] = chat
+
+    def create_chat(self, chat_name: str) -> None:
+        chat = Chat(chat_name)
+        self.chat_list[chat.get_chat_id()] = chat
 
     def handle_message(self, message: bytes) -> None:
         chat_id = message[:constants.ID_LENGTH]
-        if chat_id in self.chat_id_to_chat:
-            self.chat_id_to_chat[chat_id].handle_message(message[constants.ID_LENGTH:])
+        if chat_id in self.chat_list:
+            self.chat_list[chat_id].handle_message(message[constants.ID_LENGTH:])
