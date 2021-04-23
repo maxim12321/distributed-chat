d451ec6ea84e360d6e94c9ff40ec2add34d33466
Takumi <subarufallout@gmail.com>
2021-04-23 12:20:20 +0300

    Update file user.py

--- src/user.py
+++ src/user.py
@@ -9,13 +9,13 @@ from chat_manager import ChatManager
 class User:
     def __init__(self):
         self.user_id = os.urandom(16)
+        self.username = "Squirrel"
         self.chat_manager = ChatManager(self.user_id)
         self.message_redirection = MessageRedirection()
         self.request_redirection = RequestRedirection()
         self.byte_message_socket = ByteMessageSocket(self.message_redirection, self.request_redirection)
         self.my_ip = self.byte_message_socket.get_ip()
         self.chord = "Макс запушь ради христа"
-        self.username = "Squirrel"
 
     def create_chat(self, chat_name: str) -> None:
         self.chat_manager.create_chat(chat_name)
@@ -28,6 +28,9 @@ class User:
 
     def get_chat_list(self):
         return self.chat_manager.get_chat_list()
+
+    def get_chat_info(self, chat_id: int):
+        return self.chat_manager.get_chat_info(chat_id)
 
     def send_text_message(self, chat_id: int, message: str) -> None:
         self.chat_manager.send_text_message(chat_id, message)
