5f6744b039fbf2a16fc27399b94b89dbc25027ee
Влад Кураш <subarufallout@gmail.com>
2021-05-02 14:58:52 +0300

    Fix please

--- src/senders/local_message_sender.py
+++ src/senders/local_message_sender.py
@@ -1,6 +1,5 @@
 from typing import Optional, Callable, Dict
 
-from src.message_builders.message_builder import MessageBuilder
 from src.senders.message_sender import MessageSender
 
 
@@ -14,15 +13,7 @@ class LocalMessageSender(MessageSender):
         self.message_senders[ip] = self
 
     def send_message(self, target_ip: bytes, message: bytes) -> None:
-        message = MessageBuilder.message() \
-            .append_bytes(message) \
-            .build_with_length()
-
         self.message_senders[target_ip].handle_message(message)
 
     def send_request(self, target_ip: bytes, request: bytes) -> Optional[bytes]:
-        request = MessageBuilder.request() \
-            .append_bytes(request) \
-            .build_with_length()
-
         return self.message_senders[target_ip].handle_request(request)
