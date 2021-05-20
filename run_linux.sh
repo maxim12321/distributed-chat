e6ce8c6c58c243368e998e23c64652a14b1e4282
takumi <Rehfi14102001>
2021-05-20 17:48:32 +0300

    Add script to run application in Linux

---
+++ run_linux.sh
@@ -0,0 +1,7 @@
+# $1 - BACK port, $2 - UI port
+
+python3 flask_main.py "$1" "$2" &
+
+cd mainapp-ui
+echo "REACT_APP_BACKEND_PORT = $1" >".env.port$1"
+BACK_PORT=".env.port$1" PORT="$2" BROWSER=none yarn start_linux
